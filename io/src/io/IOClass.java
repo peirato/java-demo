7a6978dcc6c84a851cb743a43f7171635ccfeb8f
peirato <yangzeqi@xuebanginfo.com>
2019-12-03 08:41:05 +0800

    1

---
+++ io/src/io/IOClass.java
@@ -0,0 +1,10 @@
+package io;
+
+import java.io.StringReader;
+
+public class IOClass {
+
+    public static void main(String[] args) {
+        StringReader stringReader = new StringReader("content");
+    }
+}
