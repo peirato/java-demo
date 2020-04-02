115bd6efecdc57cd2482f30c0700fbe36370d2bd
杨泽琦 <peirato3@163.com>
2020-04-02 20:15:40 +0800

    java8 - optional

---
+++ java8/src/main/java/OptionalDemo.java
@@ -0,0 +1,17 @@
+import java.util.Optional;
+
+/**
+ * @author Yang zeqi
+ * @date 2020/4/2
+ * @description:
+ */
+public class OptionalDemo {
+
+    public static void main(String[] args) {
+
+        Optional.of("1").get();
+
+    }
+
+
+}
