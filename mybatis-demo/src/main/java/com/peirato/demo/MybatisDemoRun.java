d6aa2f3e8a7ffa5b246249231024dba14766d2c4
peirato <yangzeqi@xuebanginfo.com>
2021-04-18 02:19:28 +0800

    增加一个mybatis的demo

---
+++ mybatis-demo/src/main/java/com/peirato/demo/MybatisDemoRun.java
@@ -0,0 +1,47 @@
+package com.peirato.demo;
+
+import com.peirato.demo.domain.User;
+import com.peirato.demo.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MybatisDemoRun {
+
+    @Test
+    public void testFindUserById1() throws IOException {
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+
+        User o = (User)session.selectOne("com.peirato.demo.mapper.UserMapper.findUserById", 2);
+
+        System.out.println(o);
+
+    }
+
+    @Test
+    public void testFindUserById2() throws IOException {
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+
+        // 通过接口获取
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User userById = mapper.findUserById(1);
+
+        System.out.println(userById);
+
+    }
+
+
+}
---
+++ mybatis-demo/src/main/java/com/peirato/demo/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.peirato.demo.mapper;
+
+import com.peirato.demo.domain.User;
+
+public interface UserMapper {
+
+    public User findUserById(int id);
+}
